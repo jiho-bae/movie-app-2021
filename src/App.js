e1dc8bb47984c6b75a57651465b71fe49ccb760a
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-02-20 08:01:35 +0900

    Send data between components.

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react";
-import Chicken from "./Chicken";
+
+function Animal(props) {
+  return <h6>I like {props.animal}</h6>;
+}
+function Food({ fav }) {
+  return <h3>I like {fav}</h3>;
+}
 
 function App() {
   return (
     <div>
       <h1>Hello React!</h1>
-      <Chicken />
+      <Animal animal="Tiger" />
+      <Animal animal="Monkey" />
+      <Food fav="Kimchi" />
+      <Food fav="Ramyeon" />
+      <Food fav="Samgiopsal" />
     </div>
   );
 }
--- src/Chicken.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Chicken() {
-  return <h3>I love chicken</h3>;
-}
-
-export default Chicken;
