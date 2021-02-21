ac9df7591b2693f509d1cb252ae5eac5ee12a2cd
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-02-22 00:37:11 +0900

    component life cycle(Mounting, Updating, Unmounting)

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React from "react";
 import PropTypes from "prop-types";
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("hello");
+  }
   state = {
     count: 0,
   };
@@ -11,7 +15,17 @@ class App extends React.Component {
   minus = () => {
     this.setState((current) => ({ count: current.count - 1 }));
   };
+  componentDidMount() {
+    console.log("component rendered");
+  }
+  componentDidUpdate() {
+    console.log("I just Updated!");
+  }
+  componentWillUnmount() {
+    console.log("Good bye Cruel world.");
+  }
   render() {
+    console.log("I am rendering");
     return (
       <div>
         <h1>The Number is {this.state.count}</h1>
