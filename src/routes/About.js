d75f93c33b5fd67ac99722fd7f7c8a32f0b9822f
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-02-25 22:11:32 +0900

    make about-page prettier &  add detail-page. when you click the movie poster, react sends data detail-page.

--- src/routes/About.js
+++ src/routes/About.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "./About.css";
 
-function About() {
-  return <span>About Page. hi?</span>;
+function About(props) {
+  console.log(props);
+  return (
+    <div class="about__container">
+      <span>"Freedom is the freedom to say that two plus two make four. If that i granted, all else follows."</span>
+    </div>
+  );
 }
 
 export default About;
