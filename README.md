dfc5b17508f114e42e28ea03ef75c85f04bce56b
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-03-24 21:37:26 +0900

    display data on the detail page, publish in gh-pages.

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 MY first React App.
 
 React JS - make Movie App.
+
+# Preview:
+
+Try it on [gh-pages](https://jiho-bae.github.io/movie-app-2021/)
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,16 +1,36 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
     const { location, history } = this.props;
+    console.log(location);
     if (location.state === undefined) {
       history.push("/");
     }
   }
   render() {
-    const { location } = this.props;
-    if (location.state) {
-      return <span>{location.state.title}</span>;
+    const {
+      location: { state },
+    } = this.props;
+    if (state) {
+      return (
+        <div className="detail">
+          <img src={state.poster} alt={state.title} title={state.title} />
+          <div className="detail__data">
+            <span className="detail__title">{state.title}</span>
+            <span className="detail__year">{state.year}</span>
+            <ul className="detail__genres">
+              {state.genres.map((genre, index) => (
+                <li key={index} className="genres__genre">
+                  {genre}
+                </li>
+              ))}
+            </ul>
+            <p className="detail__summary">{state.summary}</p>
+          </div>
+        </div>
+      );
     } else {
       return null;
     }
