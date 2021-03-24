253dce921e30c36cae14e0b3c24dc6a9e7472a4a
jiho-bae <67041709+jiho-bae@users.noreply.github.com>
2021-03-24 20:55:18 +0900

    add navigation's css.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
-
+import "./Navigation.css";
 function Navigation() {
   return (
-    <div>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
+    <div className="Navigation">
+      <Link className="HomeLink" to="/">
+        Home
+      </Link>
+      <Link className="AboutLink" to="/about">
+        About
+      </Link>
     </div>
   );
 }
